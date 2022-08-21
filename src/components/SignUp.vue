06e1c38c608d847d1978cc535310f0c57c80bfa4
Kaung Myat Thu <kaungmyatthu111319@gmail.com>
2022-08-21 15:24:41 +0630

    add axios

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="flex items-center justify-center flex-col w-screen h-screen">
     <h1 class="text-3xl font-bold mb-4">Sign up</h1>
-    <form class="w-4/5 max-w-md bg-slate-100 px-4 py-2 rounded-md">
+    <form
+      @submit.prevent=""
+      class="w-4/5 max-w-md bg-slate-100 px-4 py-2 rounded-md"
+    >
       <input
+        v-model="name"
         type="text"
         placeholder="Enter name..."
         class="text-sky-500 border border-sky-500 w-full my-2 px-4 py-2 rounded-md focus:outline-sky-500"
       />
       <input
+        v-model="email"
         type="email"
         placeholder="Enter email..."
         class="border border-sky-500 w-full my-2 px-4 py-2 rounded-md focus:outline-sky-500"
       />
       <input
+        v-model="password"
         type="password"
         placeholder="Enter password..."
         class="border border-sky-500 w-full my-2 px-4 py-2 rounded-md focus:outline-sky-500"
       />
       <input
+        @click="signUp"
         type="submit"
         value="Sign up"
-        class="text-white bg-sky-500 w-full my-2 px-4 py-2 rounded-md hover:bg-sky-600"
+        class="text-white bg-sky-500 w-full my-2 px-4 py-2 rounded-md hover:bg-sky-600 cursor-pointer"
       />
     </form>
   </div>
@@ -29,5 +36,17 @@
 <script>
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signUp() {
+      console.log("click");
+    },
+  },
 };
 </script>
