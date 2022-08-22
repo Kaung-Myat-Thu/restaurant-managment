c32137c89856784e4f138696eb43f39fda7c7261
Kaung Myat Thu <kaungmyatthu111319@gmail.com>
2022-08-22 10:37:28 +0630

    add some styles in header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-gray-800 text-slate-300">
+    <nav class="container mx-auto flex items-center justify-between py-2">
+      <router-link to="/">
+        <img src="../assets/logo.png" alt="logo" class="w-10" />
+      </router-link>
+      <div>
+        <a class="py-4 mx-2 hover:text-slate-100" href="#">Home</a>
+        <a class="py-4 mx-2 hover:text-slate-100" href="#">Add Restaurant</a>
+        <a class="py-4 mx-2 hover:text-slate-100" href="#">Update Restaurant</a>
+        <a class="py-4 mx-2 hover:text-slate-100" href="#">Logout</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,10 +1,15 @@
 <template>
+  <Header />
   <h1>Hello, This is Home page</h1>
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 export default {
   name: "Home",
+  components: {
+    Header,
+  },
   mounted() {
     let user = localStorage.getItem("userInfo");
     if (!user) {
